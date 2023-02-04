8c6d6beac995cb45ed0bf1755d243f54db99995d
George Hilios <ghilios@gmail.com>
2023-02-03 20:52:01 -0500

    Bump version

--- NINA.Joko.Plugin.Orbitals/Properties/AssemblyInfo.cs
+++ NINA.Joko.Plugin.Orbitals/Properties/AssemblyInfo.cs
@@ -19,8 +19,8 @@ using System.Runtime.InteropServices;
 
 // [MANDATORY] The assembly versioning
 //Should be incremented for each new release build of a plugin
-[assembly: AssemblyVersion("3.0.0.5")]
-[assembly: AssemblyFileVersion("3.0.0.5")]
+[assembly: AssemblyVersion("3.0.0.6")]
+[assembly: AssemblyFileVersion("3.0.0.6")]
 
 // [MANDATORY] The name of your plugin
 [assembly: AssemblyTitle("Orbitals")]
@@ -36,7 +36,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 
 // The minimum Version of N.I.N.A. that this plugin is compatible with
-[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.0.0.1010")]
+[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.0.0.1015")]
 
 // The license your plugin code is using
 [assembly: AssemblyMetadata("License", "MPL-2.0")]
