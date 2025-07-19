b2348d6d89622044b9da57971c61941c4533b1f8
George Hilios <ghilios@gmail.com>
2025-07-19 15:39:12 -0400

    Change version to 3.1

--- NINA.Joko.Plugin.Orbitals/Properties/AssemblyInfo.cs
+++ NINA.Joko.Plugin.Orbitals/Properties/AssemblyInfo.cs
@@ -19,8 +19,8 @@ using System.Runtime.InteropServices;
 
 // [MANDATORY] The assembly versioning
 //Should be incremented for each new release build of a plugin
-[assembly: AssemblyVersion("3.0.0.17")]
-[assembly: AssemblyFileVersion("3.0.0.17")]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
 
 // [MANDATORY] The name of your plugin
 [assembly: AssemblyTitle("Orbitals")]
