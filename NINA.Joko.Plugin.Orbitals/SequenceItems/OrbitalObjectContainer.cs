dd04879c38441302753fb92a9e18d223b70653c5
George Hilios <ghilios@gmail.com>
2026-02-14 09:15:28 -0500

    Multi-object sequence does not independently update (#11)
    * Multi-object sequence does not independently update
    
    * Use blunt instrument to force the RA and Dec strings to update

--- NINA.Joko.Plugin.Orbitals/SequenceItems/OrbitalObjectContainer.cs
+++ NINA.Joko.Plugin.Orbitals/SequenceItems/OrbitalObjectContainer.cs
@@ -13,28 +13,29 @@
 using Newtonsoft.Json;
 using NINA.Astrometry;
 using NINA.Astrometry.Interfaces;
+using NINA.Core.Utility;
+using NINA.Core.Utility.Notification;
+using NINA.Joko.Plugin.Orbitals.Calculations;
+using NINA.Joko.Plugin.Orbitals.Enums;
+using NINA.Joko.Plugin.Orbitals.Interfaces;
+using NINA.Joko.Plugin.Orbitals.Utility;
+using NINA.Joko.Plugin.Orbitals.ViewModels;
 using NINA.Profile.Interfaces;
+using NINA.Sequencer.Conditions;
 using NINA.Sequencer.Container;
 using NINA.Sequencer.Container.ExecutionStrategy;
 using NINA.Sequencer.SequenceItem;
+using NINA.Sequencer.Trigger;
 using NINA.WPF.Base.Interfaces.Mediator;
 using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.ComponentModel.Composition;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Collections.ObjectModel;
-using NINA.Sequencer.Trigger;
-using NINA.Sequencer.Conditions;
-using NINA.Joko.Plugin.Orbitals.Calculations;
-using NINA.Joko.Plugin.Orbitals.Interfaces;
-using NINA.Joko.Plugin.Orbitals.Enums;
-using NINA.Joko.Plugin.Orbitals.ViewModels;
-using System.Threading;
-using NINA.Core.Utility.Notification;
-using NINA.Core.Utility;
-using System.ComponentModel;
-using NINA.Joko.Plugin.Orbitals.Utility;
 
 namespace NINA.Joko.Plugin.Orbitals.SequenceItems {
 
@@ -74,6 +75,12 @@ namespace NINA.Joko.Plugin.Orbitals.SequenceItems {
             WeakEventManager<INotifyPropertyChanged, PropertyChangedEventArgs>.AddHandler(orbitalSearchVM, nameof(orbitalSearchVM.PropertyChanged), OrbitalSearchVM_PropertyChanged);
             WeakEventManager<IOrbitalElementsAccessor, OrbitalElementsObjectTypeUpdatedEventArgs>.AddHandler(orbitalElementsAccessor, nameof(orbitalElementsAccessor.Updated), OrbitalElementsAccessor_Updated);
             PostConstruction();
+        }
+
+        [OnDeserialized]
+        internal void OnDeserializedMethod(StreamingContext context) {
+            this.RefreshCoordinates();
+            RaiseAllPropertiesChanged();
         }
 
         private void OrbitalElementsAccessor_Updated(object sender, OrbitalElementsObjectTypeUpdatedEventArgs e) {
@@ -164,9 +171,12 @@ namespace NINA.Joko.Plugin.Orbitals.SequenceItems {
             };
 
             clone.TargetObject.OrbitalElements = TargetObject.OrbitalElements;
+            clone.Target = new InputTarget(Angle.ByDegree(profileService.ActiveProfile.AstrometrySettings.Latitude), Angle.ByDegree(profileService.ActiveProfile.AstrometrySettings.Longitude), profileService.ActiveProfile.AstrometrySettings.Horizon);
             clone.Target.PositionAngle = this.Target.PositionAngle;
             clone.Target.TargetName = this.Target.TargetName;
-            clone.Target.InputCoordinates = this.Target.InputCoordinates;
+            clone.Target.InputCoordinates = this.Target.InputCoordinates.Clone();
+            clone.Target.DeepSkyObject = new OrbitalElementsObject(orbitalElementsAccessor, null, profileService.ActiveProfile.AstrometrySettings.Horizon, profileService);
+            clone.Target.DeepSkyObject.SetDateAndPosition(NighttimeCalculator.GetReferenceDate(DateTime.Now), latitude: profileService.ActiveProfile.AstrometrySettings.Latitude, longitude: profileService.ActiveProfile.AstrometrySettings.Longitude);
             clone.OrbitalSearchVM.ObjectType = this.OrbitalSearchVM.ObjectType;
             clone.OrbitalSearchVM.SetTargetNameWithoutSearch(this.OrbitalSearchVM.TargetName);
 
